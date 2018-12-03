e7a1318df6b0b634839c2d3ee34ed3cb35be5a42
Pau Rodriguez-Estivill <prodrigestivill@gmail.com>
2018-12-03 13:23:27 +0100

    backported remove throw and normalize error returns

--- index.js
+++ index.js
@@ -5,21 +5,29 @@ const bigInt = require('big-integer');
 
 module.exports = class HashidsBI extends Hashids {
 
-  encodeBI(id) {
-    // bigInt throws an exception on wrong input
-    const bi = bigInt(id, 10);
-    return this.encodeHex(bi.toString(16));
-  }
+	encodeBI(id) {
 
-  decodeBI(oid) {
-    if (typeof oid === 'string' && oid.length) {
-      const hex = this.decodeHex(oid);
-      if (typeof hex === 'string' && hex.length) {
-        const bi = bigInt(hex, 16);
-        return bi.toString(10);
-      }
-    }
-    throw new Error('Invalid id provided');
-  }
+		if (!/^[0-9]+$/.test(id)) {
+			return '';
+		}
+
+		const bi = bigInt(id, 10);
+		return this.encodeHex(bi.toString(16));
+
+	}
+
+	decodeBI(id) {
+
+		let ret = [];
+
+		const hex = this.decodeHex(id);
+		if (typeof hex === 'string' && hex.length) {
+			const bi = bigInt(hex, 16);
+			ret = bi.toString(10);
+		}
+
+		return ret;
+
+	}
 
 }
